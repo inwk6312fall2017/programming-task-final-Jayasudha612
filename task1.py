8d7790a283d0c8c5978fb4a3f46e6f571400375d
Jayasudha612 <31989835+Jayasudha612@users.noreply.github.com>
2017-10-20 14:20:12 -0300

    update commit

--- task1.py
+++ task1.py
@@ -1,21 +1,36 @@
+
 f1 = open("book1.txt")
-#word_list = []
-#accu = 0
+f2 = open("book2.txt")
+f3 = open("book3.txt")
+
+word_list = []
+
+for line1 in f1:
+
+   longest1 = ""
+   for word1 in (w for w in line1.split(" ") if len(w) % 2 == 0 and len(w) > len(longest1)):
+       longest1 = word1
+
+print(word1)
+word_list.append(word1)
+#print(word_list)
+
+
+
+
+#for line2 in f2:
+
+#   longest = ""
+#   for word in (w for w in line.split(" ") if len(w) % 2 == 0 and len(w) > len(longest)):
+#       longest = word
+
+#print(word)
+
 
 #for line in f1:
-#    word_list.append(line.strip())
 
-#for i in range(len(word_list)):
-#       print(i)
+#   longest = ""
+#   for word in (w for w in line.split(" ") if len(w) % 2 == 0 and len(w) > len(longest)):
+#       longest = word
 
-for line in f1:
-
-   longest = ""
-   for word in (w for w in line.split(" ") if len(w) % 2 == 0 and len(w) > len(longest)):
-       longest = word
-
-print(word)
-
-
-
-
+#print(word)
