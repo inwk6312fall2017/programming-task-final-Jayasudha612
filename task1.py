2be66021dce3dadd96d2ce37254008c25d3ed2f0
Jayasudha612 <31989835+Jayasudha612@users.noreply.github.com>
2017-10-20 14:04:57 -0300

    updated commit

--- task1.py
+++ task1.py
@@ -2,8 +2,8 @@ f1 = open("book1.txt")
 word_list = []
 accu = 0
 
-for line in f1:
-word_list.append(line.strip())
+#for line in f1:
+#    word_list.append(line.strip())
 
 for i in range(len(word_list)):
       print(i)
