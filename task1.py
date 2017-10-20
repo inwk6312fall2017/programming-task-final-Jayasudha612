352b56af905a0f6f655799a1b1b438c3c323c483
Jayasudha612 <31989835+Jayasudha612@users.noreply.github.com>
2017-10-20 14:05:33 -0300

    update commit

--- task1.py
+++ task1.py
@@ -1,10 +1,21 @@
 f1 = open("book1.txt")
-word_list = []
-accu = 0
+#word_list = []
+#accu = 0
 
 #for line in f1:
 #    word_list.append(line.strip())
 
-for i in range(len(word_list)):
-      print(i)
+#for i in range(len(word_list)):
+#       print(i)
+
+for line in f1:
+
+   longest = ""
+   for word in (w for w in line.split(" ") if len(w) % 2 == 0 and len(w) > len(longest)):
+       longest = word
+
+
+
+
+
 
